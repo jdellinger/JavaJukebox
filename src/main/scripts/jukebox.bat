5910b403b35be932a72ce98b663aec12ed4fb02b
Jay Dellinger <jay@dellingertechnologies.com>
2011-06-11 16:54:39 -0400

    Added batch file for starting up on windows

---
+++ src/main/scripts/jukebox.bat
@@ -0,0 +1,7 @@
+
+set "PRGDIR=%cd%"
+set "JUKEBOX_HOME=%PRGDIR%\.."
+
+set "JAVA_OPTS=-DJUKEBOX_HOME=%JUKEBOX_HOME%"
+@For %%a in ("%JUKEBOX_HOME%\lib\JavaJukebox-*.jar") do set "JARFILE=%%a"
+java %JAVA_OPTS% -jar %JARFILE% %*
